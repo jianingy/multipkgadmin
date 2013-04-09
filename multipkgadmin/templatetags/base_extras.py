8c19c0e4ab1dd995f8c9ac40320058ede0b60468
Jianing Yang <jianingy.yang@gmail.com>
2013-04-09 15:59:28 +0800

    add hide_email filter

--- multipkgadmin/templatetags/base_extras.py
+++ multipkgadmin/templatetags/base_extras.py
@@ -9,3 +9,8 @@ def active_by_name(request, name):
     if request.path.startswith(str(reverse_lazy(name))):
         return "active"
     return ""
+
+
+@register.filter(name='hide_email')
+def hide_email(value):
+    return str(value).replace('@', ' AT ').replace('.', ' DOT ')
